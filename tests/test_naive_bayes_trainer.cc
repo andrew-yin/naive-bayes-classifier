72d4d00dc7a02693720d62d58b7f8110d47606b0
Andrew Yin <andrewyin1@gmail.com>
2020-10-11 00:38:32 -0500

    Added basic test for operator overloading

--- tests/test_naive_bayes_trainer.cc
+++ tests/test_naive_bayes_trainer.cc
@@ -1,3 +1,12 @@
 #include <core/naive_bayes_trainer.h>
-
+#include <fstream>
+#include <string>
 #include <catch2/catch.hpp>
+
+
+TEST_CASE("Input stream >> operator overloading") {
+  naivebayes::NaiveBayesTrainer trainer;
+  std::ifstream training_images ("../data/mnistdatatraining/trainingimages");
+  training_images >> trainer;
+  REQUIRE(trainer.GetImageDatasetSize() == 5000);
+}
