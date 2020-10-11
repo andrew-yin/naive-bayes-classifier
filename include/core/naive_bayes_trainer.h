25ac74bfc3301c7a0fbbbdaeec440609ccf5d27d
Andrew Yin <andrewyin1@gmail.com>
2020-10-11 15:37:12 -0500

    Implemented computation of P(class = c)

--- include/core/naive_bayes_trainer.h
+++ include/core/naive_bayes_trainer.h
@@ -2,6 +2,7 @@
 #include <core/training_image.h>
 
 #include <string>
+#include <unordered_map>
 
 #include "image_dataset.h"
 #include "label_dataset.h"
@@ -12,10 +13,15 @@ class NaiveBayesTrainer {
  public:
   NaiveBayesTrainer(ImageDataset images, LabelDataset labels);
 
+  void Train();
   size_t GetImageDatasetSize();
+  double GetProbabilityClassEquals(size_t c);
 
  private:
   std::vector<TrainingImage> training_images_;
+  std::unordered_map<size_t, double> probability_class_equals_;
+
+  void ComputeProbabilitesClassEquals();
 };
 
 }  // namespace naivebayes
--- src/core/naive_bayes_trainer.cc
+++ src/core/naive_bayes_trainer.cc
@@ -13,8 +13,30 @@ NaiveBayesTrainer::NaiveBayesTrainer(ImageDataset images, LabelDataset labels) {
   }
 }
 
+void NaiveBayesTrainer::Train() {
+  ComputeProbabilitesClassEquals();
+}
+
 size_t NaiveBayesTrainer::GetImageDatasetSize() {
   return training_images_.size();
+}
+
+void NaiveBayesTrainer::ComputeProbabilitesClassEquals() {
+  std::unordered_map<size_t, size_t> class_counts;
+  for (TrainingImage image: training_images_) {
+    class_counts[image.GetLabel()]++;
+  }
+
+  size_t laplace_k = 1;
+  size_t laplace_v = class_counts.size();
+  size_t num_images = training_images_.size();
+  for (auto const &count: class_counts) {
+    probability_class_equals_[count.first] = (double) (laplace_k + count.second)/(laplace_v * laplace_k + num_images);
+  }
+}
+
+double NaiveBayesTrainer::GetProbabilityClassEquals(size_t c) {
+  return probability_class_equals_[c];
 }
 
 }  // namespace naivebayes
