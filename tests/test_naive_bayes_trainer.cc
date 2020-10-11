f72b93b4fb0a4365da4122d3839c06240044211a
Andrew Yin <andrewyin1@gmail.com>
2020-10-11 13:39:08 -0500

    Rewrote test to reflect updated project structure

--- tests/test_naive_bayes_trainer.cc
+++ tests/test_naive_bayes_trainer.cc
@@ -1,12 +1,17 @@
 #include <core/naive_bayes_trainer.h>
-#include <fstream>
-#include <string>
 #include <catch2/catch.hpp>
-
+#include <iostream>
 
 TEST_CASE("Input stream >> operator overloading") {
-  naivebayes::NaiveBayesTrainer trainer;
-  std::ifstream training_images ("../data/mnistdatatraining/trainingimages");
-  training_images >> trainer;
+  naivebayes::ImageDataset image_dataset;
+  naivebayes::LabelDataset label_dataset;
+
+  std::ifstream training_images ("data/mnistdatatraining/trainingimages");
+  std::ifstream training_labels ("data/mnistdatatraining/traininglabels");
+
+  training_images >> image_dataset;
+  training_labels >> label_dataset;
+
+  naivebayes::NaiveBayesTrainer trainer(image_dataset, label_dataset);
   REQUIRE(trainer.GetImageDatasetSize() == 5000);
 }
