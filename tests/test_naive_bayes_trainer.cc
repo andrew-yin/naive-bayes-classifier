cb1f08c9de10a8607367a2c5d19c5093367a9fed
Andrew Yin <andrewyin1@gmail.com>
2020-10-11 15:32:57 -0500

    Created tests for successful computation of P(class = c)

--- tests/test_naive_bayes_trainer.cc
+++ tests/test_naive_bayes_trainer.cc
@@ -13,5 +13,29 @@ TEST_CASE("Input stream >> operator overloading") {
   training_labels >> label_dataset;
 
   naivebayes::NaiveBayesTrainer trainer(image_dataset, label_dataset);
+
   REQUIRE(trainer.GetImageDatasetSize() == 5000);
 }
+
+TEST_CASE("Trainer class computes P(class = c) successfully") {
+  naivebayes::ImageDataset image_dataset;
+  naivebayes::LabelDataset label_dataset;
+
+  std::ifstream training_images ("data/test_data/test_images");
+  std::ifstream training_labels ("data/test_data/test_labels");
+
+  training_images >> image_dataset;
+  training_labels >> label_dataset;
+
+  naivebayes::NaiveBayesTrainer trainer(image_dataset, label_dataset);
+
+  trainer.Train();
+
+  SECTION("Test P(class = 0)") {
+    REQUIRE(trainer.GetProbabilityClassEquals(0) == Approx(0.4));
+  }
+  SECTION("Test P(class = 1)") {
+    REQUIRE(trainer.GetProbabilityClassEquals(1) == Approx(0.6));
+  }
+}
+
