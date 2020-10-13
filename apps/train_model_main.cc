9659a831643854e7e63c25939dd6b67fa71adc8e
Andrew Yin <andrewyin1@gmail.com>
2020-10-13 12:10:38 -0500

    Refactored main for readability

--- apps/train_model_main.cc
+++ apps/train_model_main.cc
@@ -3,6 +3,13 @@
 
 #include <iostream>
 
+bool DetermineIsTraining();
+bool DetermineIsLoading();
+bool DetermineIsSaving();
+naivebayes::NaiveBayesModel GenerateModel(bool const &is_training,
+                                          bool const &is_loading,
+                                          bool const &is_saving);
+
 DEFINE_string(training_image, "null",
               "The file path containing a training image dataset");
 DEFINE_string(training_label, "null",
@@ -13,47 +20,26 @@ DEFINE_string(load, "null", "The file path to load the model from");
 int main(int argc, char **argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-  bool train_from_files;
-  bool load_from_file;
-  bool save_to_file;
+  bool is_training = DetermineIsTraining();
+  bool is_loading = DetermineIsLoading();
+  bool is_saving = DetermineIsSaving();
 
-  if (FLAGS_training_image == "null") {
-    if (FLAGS_training_label != "null") {
-      throw std::invalid_argument(
-          "Error: Model must be trained using both images and labels, but you "
-          "have only provided a path to labels. Please try again.");
-    } else {
-      train_from_files = false;
-    }
-  } else {
-    if (FLAGS_training_label == "null") {
-      throw std::invalid_argument(
-          "Error: Model must be trained using both images and labels, but you "
-          "have only provided a path to images. Please try again.");
-    } else {
-      train_from_files = true;
-    }
-  }
-
-  if (FLAGS_load == "null") {
-    load_from_file = false;
-  } else {
-    load_from_file = true;
-  }
-
-  if (FLAGS_save == "null") {
-    save_to_file = false;
-  } else {
-    save_to_file = true;
-  }
-
-  if (train_from_files && load_from_file) {
+  if (is_training && is_loading) {
     throw std::invalid_argument(
         "Error: Model cannot both be loaded from a file and trained with a "
         "dataset. Please try again.");
   }
 
-  if (train_from_files) {
+  naivebayes::NaiveBayesModel model =
+      GenerateModel(is_training, is_loading, is_saving);
+
+  return 0;
+}
+
+naivebayes::NaiveBayesModel GenerateModel(bool const &is_training,
+                                          bool const &is_loading,
+                                          bool const &is_saving) {
+  if (is_training) {
     naivebayes::ImageDataset image_dataset;
     naivebayes::LabelDataset label_dataset;
 
@@ -65,21 +51,62 @@ int main(int argc, char **argv) {
     naivebayes::NaiveBayesModel model(image_dataset, label_dataset);
     model.Train();
 
-    if (save_to_file) {
+    if (is_saving) {
       std::ofstream save_file(FLAGS_save);
       save_file << model;
     }
-  } else if (load_from_file) {
+
+    return model;
+
+  } else if (is_loading) {
     naivebayes::NaiveBayesModel model;
 
     std::ifstream load_file(FLAGS_load);
     load_file >> model;
 
-    if (save_to_file) {
+    if (is_saving) {
       std::ofstream save_file(FLAGS_save);
       save_file << model;
     }
-  }
 
-  return 0;
+    return model;
+
+  } else {
+    throw std::invalid_argument(
+        "Error: Model cannot be generated. Input data not found.");
+  }
+}
+
+bool DetermineIsSaving() {
+  if (FLAGS_save == "null") {
+    return false;
+  } else {
+    return true;
+  }
+}
+bool DetermineIsLoading() {
+  if (FLAGS_load == "null") {
+    return false;
+  } else {
+    return true;
+  }
+}
+bool DetermineIsTraining() {
+  if (FLAGS_training_image == "null") {
+    if (FLAGS_training_label != "null") {
+      throw std::invalid_argument(
+          "Error: Model must be trained using both images and labels, but you "
+          "have only provided a path to labels. Please try again.");
+    } else {
+      return false;
+    }
+  } else {
+    if (FLAGS_training_label == "null") {
+      throw std::invalid_argument(
+          "Error: Model must be trained using both images and labels, but you "
+          "have only provided a path to images. Please try again.");
+    } else {
+      return true;
+    }
+  }
 }
