28f19ac12bb67979c5835639bc54a0a3c2161587
Andrew Yin <andrewyin1@gmail.com>
2020-10-12 21:06:52 -0500

    Updated operator overloading test case for increased specificity

--- tests/test_naive_bayes_trainer.cc
+++ tests/test_naive_bayes_trainer.cc
@@ -3,26 +3,45 @@
 #include <catch2/catch.hpp>
 #include <iostream>
 
-naivebayes::ImageDataset image_dataset;
-naivebayes::LabelDataset label_dataset;
-
-std::ifstream training_images("data/test_data/test_images");
-std::ifstream training_labels("data/test_data/test_labels");
-
-//TODO: create more specific tests
 TEST_CASE("Input stream >> operator overloading") {
-  training_images >> image_dataset;
-  training_labels >> label_dataset;
+  naivebayes::ImageDataset image_dataset;
+  naivebayes::LabelDataset label_dataset;
 
-  naivebayes::NaiveBayesTrainer trainer(image_dataset, label_dataset);
-  trainer.Train();
+  std::ifstream test_image_stream("data/test_data_3x3/test_images");
+  std::ifstream test_label_stream("data/test_data_3x3/test_labels");
 
-  REQUIRE(trainer.GetImageDatasetSize() == 3);
+  test_image_stream >> image_dataset;
+  test_label_stream >> label_dataset;
+
+  naivebayes::Image image1 = {
+      {'#', '+', ' '}, {' ', '+', ' '}, {'+', '+', '#'}};
+
+  naivebayes::Image image2 = {
+      {'#', '+', '#'}, {'+', ' ', '+'}, {'#', '#', '+'}};
+
+  naivebayes::Image image3 = {
+      {' ', '+', ' '}, {' ', '#', ' '}, {' ', '+', ' '}};
+
+  std::vector<naivebayes::Image> images = {image1, image2, image3};
+  std::vector<size_t> labels = {1, 0, 1};
+
+  SECTION("Test that image contents are correct") {
+    REQUIRE(images == image_dataset.images_);
+  }
+  SECTION("Test that label contents are correct") {
+    REQUIRE(labels == label_dataset.labels_);
+  }
 }
 
 TEST_CASE("Trainer class computes P(class = c) successfully") {
-  training_images >> image_dataset;
-  training_labels >> label_dataset;
+  naivebayes::ImageDataset image_dataset;
+  naivebayes::LabelDataset label_dataset;
+
+  std::ifstream test_image_stream("data/test_data_3x3/test_images");
+  std::ifstream test_label_stream("data/test_data_3x3/test_labels");
+
+  test_image_stream >> image_dataset;
+  test_label_stream >> label_dataset;
 
   naivebayes::NaiveBayesTrainer trainer(image_dataset, label_dataset);
   trainer.Train();
@@ -36,8 +55,14 @@ TEST_CASE("Trainer class computes P(class = c) successfully") {
 }
 
 TEST_CASE("Trainer class computes P(F_(i,j) = f | class = c) successfully") {
-  training_images >> image_dataset;
-  training_labels >> label_dataset;
+  naivebayes::ImageDataset image_dataset;
+  naivebayes::LabelDataset label_dataset;
+
+  std::ifstream test_image_stream("data/test_data_3x3/test_images");
+  std::ifstream test_label_stream("data/test_data_3x3/test_labels");
+
+  test_image_stream >> image_dataset;
+  test_label_stream >> label_dataset;
 
   naivebayes::NaiveBayesTrainer trainer(image_dataset, label_dataset);
   trainer.Train();
@@ -81,120 +106,125 @@ TEST_CASE("Trainer class computes P(F_(i,j) = f | class = c) successfully") {
 
   SECTION("Test P(F(0,0) = 1 | class = 0)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(0, 0, 1, 0) ==
-        Approx(0.666667));
+            Approx(0.666667));
   }
   SECTION("Test P(F(0,1) = 1 | class = 0)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(0, 1, 1, 0) ==
-        Approx(0.666667));
+            Approx(0.666667));
   }
   SECTION("Test P(F(0,2) = 1 | class = 0)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(0, 2, 1, 0) ==
-        Approx(0.666667));
+            Approx(0.666667));
   }
   SECTION("Test P(F(1,0) = 1 | class = 0)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(1, 0, 1, 0) ==
-        Approx(0.666667));
+            Approx(0.666667));
   }
   SECTION("Test P(F(1,1) = 1 | class = 0)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(1, 1, 1, 0) ==
-        Approx(0.333333));
+            Approx(0.333333));
   }
   SECTION("Test P(F(1,2) = 1 | class = 0)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(1, 2, 1, 0) ==
-        Approx(0.666667));
+            Approx(0.666667));
   }
   SECTION("Test P(F(2,0) = 1 | class = 0)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(2, 0, 1, 0) ==
-        Approx(0.666667));
+            Approx(0.666667));
   }
   SECTION("Test P(F(2,1) = 1 | class = 0)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(2, 1, 1, 0) ==
-        Approx(0.666667));
+            Approx(0.666667));
   }
   SECTION("Test P(F(2,2) = 1 | class = 0)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(2, 2, 1, 0) ==
-        Approx(0.666667));
+            Approx(0.666667));
   }
 
   SECTION("Test P(F(0,0) = 0 | class = 1)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(0, 0, 0, 1) ==
-        Approx(0.5));
+            Approx(0.5));
   }
   SECTION("Test P(F(0,1) = 0 | class = 1)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(0, 1, 0, 1) ==
-        Approx(0.25));
+            Approx(0.25));
   }
   SECTION("Test P(F(0,2) = 0 | class = 1)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(0, 2, 0, 1) ==
-        Approx(0.75));
+            Approx(0.75));
   }
   SECTION("Test P(F(1,0) = 0 | class = 1)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(1, 0, 0, 1) ==
-        Approx(0.75));
+            Approx(0.75));
   }
   SECTION("Test P(F(1,1) = 0 | class = 1)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(1, 1, 0, 1) ==
-        Approx(0.25));
+            Approx(0.25));
   }
   SECTION("Test P(F(1,2) = 0 | class = 1)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(1, 2, 0, 1) ==
-        Approx(0.75));
+            Approx(0.75));
   }
   SECTION("Test P(F(2,0) = 0 | class = 1)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(2, 0, 0, 1) ==
-        Approx(0.5));
+            Approx(0.5));
   }
   SECTION("Test P(F(2,1) = 0 | class = 1)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(2, 1, 0, 1) ==
-        Approx(0.25));
+            Approx(0.25));
   }
   SECTION("Test P(F(2,2) = 0 | class = 1)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(2, 2, 0, 1) ==
-        Approx(0.5));
+            Approx(0.5));
   }
-
 
   SECTION("Test P(F(0,0) = 1 | class = 1)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(0, 0, 1, 1) ==
-        Approx(0.5));
+            Approx(0.5));
   }
   SECTION("Test P(F(0,1) = 1 | class = 1)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(0, 1, 1, 1) ==
-        Approx(0.75));
+            Approx(0.75));
   }
   SECTION("Test P(F(0,2) = 1 | class = 1)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(0, 2, 1, 1) ==
-        Approx(0.25));
+            Approx(0.25));
   }
   SECTION("Test P(F(1,0) = 1 | class = 1)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(1, 0, 1, 1) ==
-        Approx(0.25));
+            Approx(0.25));
   }
   SECTION("Test P(F(1,1) = 1 | class = 1)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(1, 1, 1, 1) ==
-        Approx(0.75));
+            Approx(0.75));
   }
   SECTION("Test P(F(1,2) = 1 | class = 1)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(1, 2, 1, 1) ==
-        Approx(0.25));
+            Approx(0.25));
   }
   SECTION("Test P(F(2,0) = 1 | class = 1)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(2, 0, 1, 1) ==
-        Approx(0.5));
+            Approx(0.5));
   }
   SECTION("Test P(F(2,1) = 1 | class = 1)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(2, 1, 1, 1) ==
-        Approx(0.75));
+            Approx(0.75));
   }
   SECTION("Test P(F(2,2) = 1 | class = 1)") {
     REQUIRE(trainer.GetProbabilityPixelEqualsGivenClass(2, 2, 1, 1) ==
-        Approx(0.5));
+            Approx(0.5));
   }
 }
 
 TEST_CASE("Trainer class can save and load model to/from file") {
-  training_images >> image_dataset;
-  training_labels >> label_dataset;
+  naivebayes::ImageDataset image_dataset;
+  naivebayes::LabelDataset label_dataset;
+
+  std::ifstream test_image_stream("data/test_data_3x3/test_images");
+  std::ifstream test_label_stream("data/test_data_3x3/test_labels");
+
+  test_image_stream >> image_dataset;
+  test_label_stream >> label_dataset;
 
   naivebayes::NaiveBayesTrainer trainer(image_dataset, label_dataset);
   trainer.Train();
@@ -218,148 +248,148 @@ TEST_CASE("Trainer class can save and load model to/from file") {
 
   SECTION("Test P(F(0,0) = 0 | class = 0)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(0, 0, 0, 0) ==
-        Approx(0.333333));
+            Approx(0.333333));
   }
   SECTION("Test P(F(0,1) = 0 | class = 0)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(0, 1, 0, 0) ==
-        Approx(0.333333));
+            Approx(0.333333));
   }
   SECTION("Test P(F(0,2) = 0 | class = 0)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(0, 2, 0, 0) ==
-        Approx(0.333333));
+            Approx(0.333333));
   }
   SECTION("Test P(F(1,0) = 0 | class = 0)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(1, 0, 0, 0) ==
-        Approx(0.333333));
+            Approx(0.333333));
   }
   SECTION("Test P(F(1,1) = 0 | class = 0)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(1, 1, 0, 0) ==
-        Approx(0.666667));
+            Approx(0.666667));
   }
   SECTION("Test P(F(1,2) = 0 | class = 0)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(1, 2, 0, 0) ==
-        Approx(0.333333));
+            Approx(0.333333));
   }
   SECTION("Test P(F(2,0) = 0 | class = 0)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(2, 0, 0, 0) ==
-        Approx(0.333333));
+            Approx(0.333333));
   }
   SECTION("Test P(F(2,1) = 0 | class = 0)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(2, 1, 0, 0) ==
-        Approx(0.333333));
+            Approx(0.333333));
   }
   SECTION("Test P(F(2,2) = 0 | class = 0)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(2, 2, 0, 0) ==
-        Approx(0.333333));
+            Approx(0.333333));
   }
 
   SECTION("Test P(F(0,0) = 1 | class = 0)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(0, 0, 1, 0) ==
-        Approx(0.666667));
+            Approx(0.666667));
   }
   SECTION("Test P(F(0,1) = 1 | class = 0)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(0, 1, 1, 0) ==
-        Approx(0.666667));
+            Approx(0.666667));
   }
   SECTION("Test P(F(0,2) = 1 | class = 0)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(0, 2, 1, 0) ==
-        Approx(0.666667));
+            Approx(0.666667));
   }
   SECTION("Test P(F(1,0) = 1 | class = 0)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(1, 0, 1, 0) ==
-        Approx(0.666667));
+            Approx(0.666667));
   }
   SECTION("Test P(F(1,1) = 1 | class = 0)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(1, 1, 1, 0) ==
-        Approx(0.333333));
+            Approx(0.333333));
   }
   SECTION("Test P(F(1,2) = 1 | class = 0)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(1, 2, 1, 0) ==
-        Approx(0.666667));
+            Approx(0.666667));
   }
   SECTION("Test P(F(2,0) = 1 | class = 0)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(2, 0, 1, 0) ==
-        Approx(0.666667));
+            Approx(0.666667));
   }
   SECTION("Test P(F(2,1) = 1 | class = 0)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(2, 1, 1, 0) ==
-        Approx(0.666667));
+            Approx(0.666667));
   }
   SECTION("Test P(F(2,2) = 1 | class = 0)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(2, 2, 1, 0) ==
-        Approx(0.666667));
+            Approx(0.666667));
   }
 
   SECTION("Test P(F(0,0) = 0 | class = 1)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(0, 0, 0, 1) ==
-        Approx(0.5));
+            Approx(0.5));
   }
   SECTION("Test P(F(0,1) = 0 | class = 1)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(0, 1, 0, 1) ==
-        Approx(0.25));
+            Approx(0.25));
   }
   SECTION("Test P(F(0,2) = 0 | class = 1)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(0, 2, 0, 1) ==
-        Approx(0.75));
+            Approx(0.75));
   }
   SECTION("Test P(F(1,0) = 0 | class = 1)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(1, 0, 0, 1) ==
-        Approx(0.75));
+            Approx(0.75));
   }
   SECTION("Test P(F(1,1) = 0 | class = 1)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(1, 1, 0, 1) ==
-        Approx(0.25));
+            Approx(0.25));
   }
   SECTION("Test P(F(1,2) = 0 | class = 1)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(1, 2, 0, 1) ==
-        Approx(0.75));
+            Approx(0.75));
   }
   SECTION("Test P(F(2,0) = 0 | class = 1)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(2, 0, 0, 1) ==
-        Approx(0.5));
+            Approx(0.5));
   }
   SECTION("Test P(F(2,1) = 0 | class = 1)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(2, 1, 0, 1) ==
-        Approx(0.25));
+            Approx(0.25));
   }
   SECTION("Test P(F(2,2) = 0 | class = 1)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(2, 2, 0, 1) ==
-        Approx(0.5));
+            Approx(0.5));
   }
   SECTION("Test P(F(0,0) = 1 | class = 1)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(0, 0, 1, 1) ==
-        Approx(0.5));
+            Approx(0.5));
   }
   SECTION("Test P(F(0,1) = 1 | class = 1)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(0, 1, 1, 1) ==
-        Approx(0.75));
+            Approx(0.75));
   }
   SECTION("Test P(F(0,2) = 1 | class = 1)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(0, 2, 1, 1) ==
-        Approx(0.25));
+            Approx(0.25));
   }
   SECTION("Test P(F(1,0) = 1 | class = 1)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(1, 0, 1, 1) ==
-        Approx(0.25));
+            Approx(0.25));
   }
   SECTION("Test P(F(1,1) = 1 | class = 1)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(1, 1, 1, 1) ==
-        Approx(0.75));
+            Approx(0.75));
   }
   SECTION("Test P(F(1,2) = 1 | class = 1)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(1, 2, 1, 1) ==
-        Approx(0.25));
+            Approx(0.25));
   }
   SECTION("Test P(F(2,0) = 1 | class = 1)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(2, 0, 1, 1) ==
-        Approx(0.5));
+            Approx(0.5));
   }
   SECTION("Test P(F(2,1) = 1 | class = 1)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(2, 1, 1, 1) ==
-        Approx(0.75));
+            Approx(0.75));
   }
   SECTION("Test P(F(2,2) = 1 | class = 1)") {
     REQUIRE(trainer2.GetProbabilityPixelEqualsGivenClass(2, 2, 1, 1) ==
-        Approx(0.5));
+            Approx(0.5));
   }
 }
