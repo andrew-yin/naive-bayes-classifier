20e89b809fb9848a10988111b311851bc04837a4
Andrew Yin <andrewyin1@gmail.com>
2020-10-18 23:04:21 -0500

    Added necessary data members for classifier

--- include/core/naive_bayes_classifier.h
+++ include/core/naive_bayes_classifier.h
@@ -1,12 +1,20 @@
 #pragma once
 
+#include "naive_bayes_model.h"
+
 namespace naivebayes {
 
 class NaiveBayesClassifier {
  public:
-
+  NaiveBayesClassifier(const NaiveBayesModel& model, const Image& image);
+  size_t Classify();
+  double GetLikelihoodScore(size_t digit);
  private:
+  NaiveBayesModel model_;
+  Image image_;
+  std::unordered_map<size_t, double> likelihood_scores_;
 
+  void ComputeLikelihoodScores();
 };
 
-}
+}  // namespace naivebayes
