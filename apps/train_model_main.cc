9facb46443546c450ee7909dd6a6a06199e6cb74
Andrew Yin <andrewyin1@gmail.com>
2020-10-20 15:03:30 -0500

    Refactored testing method in main

--- apps/train_model_main.cc
+++ apps/train_model_main.cc
@@ -4,6 +4,8 @@
 
 #include <iostream>
 
+using namespace naivebayes;
+
 DEFINE_string(training_images, "null",
               "The file path containing a training image dataset");
 DEFINE_string(training_labels, "null",
@@ -19,11 +21,12 @@ DEFINE_string(test_labels, "null",
 bool DetermineIsTraining();
 
 /* Create a model based on whether it needs to be trained/loaded/saved */
-naivebayes::NaiveBayesModel GenerateModel(bool training, bool loading,
-                                          bool saving);
+NaiveBayesModel GenerateModel(bool training, bool loading, bool saving);
+
+void TestModel(const NaiveBayesModel& model);
 
 bool DetermineIsTesting();
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   /* True if the model should be trained from datasets, false otherwise */
   bool is_training = DetermineIsTraining();
@@ -45,63 +48,31 @@ int main(int argc, char **argv) {
               << std::endl;
   }
 
-  naivebayes::NaiveBayesModel model =
-      GenerateModel(is_training, is_loading, is_saving);
+  NaiveBayesModel model = GenerateModel(is_training, is_loading, is_saving);
 
   if (is_testing) {
-    std::cout << "Testing model..." << std::endl;
-    naivebayes::ImageDataset test_images;
-    naivebayes::LabelDataset test_labels;
-
-    std::ifstream image_stream(FLAGS_test_images);
-    std::ifstream label_stream(FLAGS_test_labels);
-    image_stream >> test_images;
-    label_stream >> test_labels;
-
-    size_t image_dataset_size = test_images.images_.size();
-    size_t label_dataset_size = test_labels.labels_.size();
-    if (image_dataset_size != label_dataset_size) {
-      throw std::invalid_argument(
-          "Test image and label datasets given do not align. Please try "
-          "again.");
-    } else if (image_dataset_size == 0 || label_dataset_size == 0) {
-      throw std::invalid_argument(
-          "Test files specified are either blank or DNE."
-          "Please try again.");
-    }
-
-    size_t num_correct = 0;
-    naivebayes::NaiveBayesClassifier classifier(model);
-    for (size_t i = 0; i < test_images.images_.size(); i++) {
-      if (classifier.Classify(test_images.images_[i]) == test_labels.labels_[i]) {
-        num_correct++;
-      }
-    }
-
-    double accuracy = (double)num_correct / test_images.images_.size() * 100.0;
-    std::cout << "Your model determined the digit correctly " << accuracy
-              << "% of the time." << std::endl;
+    TestModel(model);
   }
 
   return 0;
 }
 
-naivebayes::NaiveBayesModel GenerateModel(bool is_training, bool is_loading,
-                                          bool is_saving) {
+NaiveBayesModel GenerateModel(bool is_training, bool is_loading,
+                              bool is_saving) {
   if (is_training) {
     std::cout << "Loading training images from \"" << FLAGS_training_images
               << "\"..." << std::endl;
-    naivebayes::ImageDataset image_dataset;
+    ImageDataset image_dataset;
     std::ifstream image_stream(FLAGS_training_images);
     image_stream >> image_dataset;
 
     std::cout << "Loading training labels from \"" << FLAGS_training_labels
               << "\"..." << std::endl;
-    naivebayes::LabelDataset label_dataset;
+    LabelDataset label_dataset;
     std::ifstream label_stream(FLAGS_training_labels);
     label_stream >> label_dataset;
 
-    naivebayes::NaiveBayesModel model(image_dataset, label_dataset);
+    NaiveBayesModel model(image_dataset, label_dataset);
     std::cout << "Loaded training dataset successfully." << std::endl;
 
     std::cout << "Training model..." << std::endl;
@@ -119,7 +90,7 @@ naivebayes::NaiveBayesModel GenerateModel(bool is_training, bool is_loading,
     return model;
   } else if (is_loading) {
     std::cout << "Loading model from \"" << FLAGS_load << "\"..." << std::endl;
-    naivebayes::NaiveBayesModel model;
+    NaiveBayesModel model;
     std::ifstream load_file(FLAGS_load);
     load_file >> model;
     std::cout << "Model loaded successfully." << std::endl;
@@ -176,4 +147,39 @@ bool DetermineIsTesting() {
       return true;
     }
   }
+}
+
+void TestModel(const NaiveBayesModel& model) {
+  std::cout << "Testing model..." << std::endl;
+  ImageDataset test_images;
+  LabelDataset test_labels;
+
+  std::ifstream image_stream(FLAGS_test_images);
+  std::ifstream label_stream(FLAGS_test_labels);
+  image_stream >> test_images;
+  label_stream >> test_labels;
+
+  size_t image_dataset_size = test_images.images_.size();
+  size_t label_dataset_size = test_labels.labels_.size();
+  if (image_dataset_size != label_dataset_size) {
+    throw std::invalid_argument(
+        "Test image and label datasets given do not align. Please try "
+        "again.");
+  } else if (image_dataset_size == 0 || label_dataset_size == 0) {
+    throw std::invalid_argument(
+        "Test files specified are either blank or DNE."
+        "Please try again.");
+  }
+
+  size_t num_correct = 0;
+  NaiveBayesClassifier classifier(model);
+  for (size_t i = 0; i < test_images.images_.size(); i++) {
+    if (classifier.Classify(test_images.images_[i]) == test_labels.labels_[i]) {
+      num_correct++;
+    }
+  }
+
+  double accuracy = (double)num_correct / test_images.images_.size() * 100.0;
+  std::cout << "Your model determined the digit correctly " << accuracy
+            << "% of the time." << std::endl;
 }
