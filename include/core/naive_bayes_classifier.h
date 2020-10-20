4f578d0a86f9a650c0f32efa232260e05319129c
Andrew Yin <andrewyin1@gmail.com>
2020-10-20 00:30:58 -0500

    Refactored classifier to allow for repeated use of model

--- include/core/naive_bayes_classifier.h
+++ include/core/naive_bayes_classifier.h
@@ -6,15 +6,15 @@ namespace naivebayes {
 
 class NaiveBayesClassifier {
  public:
-  NaiveBayesClassifier(const NaiveBayesModel& model, const Image& image);
-  size_t Classify();
-  double GetLikelihoodScore(size_t digit);
+  NaiveBayesClassifier(const NaiveBayesModel& model);
+  size_t Classify(const Image& image);
+  double GetLikelihoodScore(const size_t& digit, const Image& image);
+
  private:
   NaiveBayesModel model_;
-  Image image_;
-  std::unordered_map<size_t, double> likelihood_scores_;
 
-  void ComputeLikelihoodScores();
+  std::unordered_map<size_t, double> ComputeLikelihoodScores(
+      const Image& image);
 };
 
 }  // namespace naivebayes
--- src/core/naive_bayes_classifier.cc
+++ src/core/naive_bayes_classifier.cc
@@ -4,16 +4,16 @@
 
 namespace naivebayes {
 
-NaiveBayesClassifier::NaiveBayesClassifier(const NaiveBayesModel &model,
-                                           const Image &image)
-    : model_(model), image_(image) {
+NaiveBayesClassifier::NaiveBayesClassifier(const NaiveBayesModel& model)
+    : model_(model) {
 }
 
-size_t NaiveBayesClassifier::Classify() {
-  ComputeLikelihoodScores();
+size_t NaiveBayesClassifier::Classify(const Image& image) {
+  std::unordered_map<size_t, double> likelihood_scores =
+      ComputeLikelihoodScores(image);
   size_t most_likely_digit = -1;
   double highest_likelihood_score = -DBL_MAX;
-  for (const auto &i : likelihood_scores_) {
+  for (const auto &i : likelihood_scores) {
     if (highest_likelihood_score < i.second) {
       most_likely_digit = i.first;
       highest_likelihood_score = i.second;
@@ -22,22 +22,29 @@ size_t NaiveBayesClassifier::Classify() {
   return most_likely_digit;
 }
 
-double NaiveBayesClassifier::GetLikelihoodScore(size_t digit) {
-  return likelihood_scores_[digit];
+double NaiveBayesClassifier::GetLikelihoodScore(const size_t& digit,
+                                                const Image& image) {
+  std::unordered_map<size_t, double> likelihood_scores =
+      ComputeLikelihoodScores(image);
+  return likelihood_scores[digit];
 }
 
-void NaiveBayesClassifier::ComputeLikelihoodScores() {
-  for (size_t &digit: model_.GetClasses()) {
+std::unordered_map<size_t, double>
+NaiveBayesClassifier::ComputeLikelihoodScores(const Image& image) {
+  std::unordered_map<size_t, double> likelihood_scores;
+  for (size_t& digit : model_.GetClasses()) {
     double likelihood_score = log10(model_.GetClassProbability(digit));
-    for (size_t row = 0; row < image_.size(); row++) {
-      for (size_t col = 0; col < image_.size(); col++) {
-        bool is_shaded = image_[row][col] != ' ';
+    for (size_t row = 0; row < image.size(); row++) {
+      for (size_t col = 0; col < image.size(); col++) {
+        bool is_shaded = image[row][col] != ' ';
         likelihood_score +=
             log10(model_.GetPixelProbability(row, col, is_shaded, digit));
       }
     }
-    likelihood_scores_[digit] = likelihood_score;
+    likelihood_scores[digit] = likelihood_score;
   }
+
+  return likelihood_scores;
 }
 
 }  // namespace naivebayes
